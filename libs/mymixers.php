f76bfbe1355883843cf1441566c2d718221e256e
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-08-11 12:59:40 +0200

    added mixer methods

---
+++ libs/mymixers.php
@@ -0,0 +1,34 @@
+<?php
+class myMixers
+{
+    public function getMixer($mixer = '')
+    {
+        return $this->$mixer;
+    }
+
+    private function setMixer($mixer, $channel = '', $key = '') // $key if is mixer edition
+    {
+        if (empty($key)) {
+            if (is_array($channel)) {
+                if (is_array($this->$mixer)) {
+                    $key = key(end($this->$mixer)) + 1;
+                    $this->$mixer += array($key => (object) $channel);
+                }
+                else {
+                    $this->$mixer = array( (object) $channel);
+                }
+            }
+                
+        }
+        else {
+            $this->$mixer = array($key => (object) $channel);
+        }
+    }
+
+    public function addChannel($mixer, $soundCard, $soundCardMixer, $soundCardChannel)
+    {
+        $elements = array('soundCard' => $soundCard, 'soundCardMixer' => $soundCardMixer, 'soundCardChannel' => $soundCardChannel);
+        $this->setMixer($mixer, $elements);
+    }
+}
+?>
