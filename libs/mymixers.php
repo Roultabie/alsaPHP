7bfed6e5de386d2556c172e3810eba485a991769
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-08-13 10:21:34 +0200

    updated mymixers class

--- libs/mymixers.php
+++ libs/mymixers.php
@@ -1,9 +1,22 @@
 <?php
 class myMixers
 {
+
+    private static $myMixers;
+
+    function __construct()
+    {
+        $this->arrayToObject($myMixers);
+    }
+
     public function getMixer($mixer)
     {
         return $this->$mixer;
+    }
+
+    public function getMixers($mixers = '')
+    {
+
     }
 
     private function setMixer($mixer, $channel = '', $key = '') // $key if is mixer edition
@@ -25,10 +38,31 @@ class myMixers
         }
     }
 
-    public function addChannel($mixer, $soundCardNumber, $soundCardMixer, $soundCardChannel)
+    private function addChannel($mixer, $soundCardNumber, $soundCardMixer, $soundCardChannel)
     {
         $elements = array('soundCardNumber' => $soundCardNumber, 'soundCardMixer' => $soundCardMixer, 'soundCardChannel' => $soundCardChannel);
         $this->setMixer($mixer, $elements);
+    }
+
+    private function arrayToObject()
+    {
+        if (is_array(self::$myMixers)) {
+            foreach (self::$myMixers as $mixerName => $mixers) {
+                foreach ($mixers as $element) {
+                    $this->addChannel($mixerName, $element['soundCardNumber'], $element['soundCardMixer'], $element['soundCardChannel']);
+                }
+            }
+        }
+    }
+
+    public static function getMyMixers()
+    {
+        return self::$myMixers;
+    }
+
+    public static function setMyMixers($myMixers)
+    {
+        self::$myMixers = $myMixers;
     }
 }
 ?>
