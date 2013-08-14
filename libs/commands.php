6cf6518c5b182aa40a9e78eab0f2e6a625451861
Daniel DOUAT <daniel.douat@aelys-info.fr>
2013-08-14 18:34:26 +0200

    added volume commands

--- libs/commands.php
+++ libs/commands.php
@@ -4,7 +4,101 @@ extends soundCard
 {
     function __construct($soundCardNumber)
     {
-        $this->soundCard = (int) $soundCardNumber;
+        $this->soundCard  = (int) $soundCardNumber;
+        $this->volumeStep = '3';
+    }
+
+    public function setVolumeUp($mixer, $channel = '')
+    {
+        $exec       = 'sget "' . $mixer . '"';
+        $execResult = $this->amixer($exec);
+        $channels   = self::listSoundCardChannels($this->soundCard, $mixer);
+        $channels   = implode('|', $channels);
+        $pattern    ='/\s+([' . $channels . ']+):[\w\d\s]*\[(\d+)%\]/';
+        preg_match_all($pattern, $execResult, $matches);
+        $result['mixer'] = $mixer;
+        if (is_array($matches[1])) {
+            foreach ($matches[1] as $key => $value) {
+                if (!empty($channel)) {
+                    if (in_array($value, $channel)) {
+                        $commandLine  .= $this->volumeStep . '%+,';
+                    }
+                    else {
+                        $commandLine .= $matches[2][$key] . '%,';
+                    }
+                }
+                else {
+                    $commandLine  .= $this->volumeStep . '%+,';
+                }
+            }
+            $commandLine = rtrim($commandLine, ',');
+            echo $commandLine;
+        }
+        $exec = 'sset "' . $mixer . '" ' . $commandLine;
+        $execResult = $this->amixer($exec);
+        return $execResult;
+    }
+
+    public function setVolumeDown($mixer, $channel = '')
+    {
+        $exec       = 'sget "' . $mixer . '"';
+        $execResult = $this->amixer($exec);
+        $channels   = self::listSoundCardChannels($this->soundCard, $mixer);
+        $channels   = implode('|', $channels);
+        $pattern    ='/\s+([' . $channels . ']+):[\w\d\s]*\[(\d+)%\]/';
+        preg_match_all($pattern, $execResult, $matches);
+        $result['mixer'] = $mixer;
+        if (is_array($matches[1])) {
+            foreach ($matches[1] as $key => $value) {
+                if (!empty($channel)) {
+                    if (in_array($value, $channel)) {
+                        $commandLine  .= $this->volumeStep . '%-,';
+                    }
+                    else {
+                        $commandLine .= $matches[2][$key] . '%,';
+                    }
+                }
+                else {
+                    $commandLine  .= $this->volumeStep . '%-,';
+                }
+            }
+            $commandLine = rtrim($commandLine, ',');
+            echo $commandLine;
+        }
+        $exec = 'sset "' . $mixer . '" ' . $commandLine;
+        $execResult = $this->amixer($exec);
+        return $execResult;
+    }
+
+    public function setVolume($volume, $mixer, $channel = '')
+    {
+        $exec       = 'sget "' . $mixer . '"';
+        $execResult = $this->amixer($exec);
+        $channels   = self::listSoundCardChannels($this->soundCard, $mixer);
+        $channels   = implode('|', $channels);
+        $pattern    ='/\s+([' . $channels . ']+):[\w\d\s]*\[(\d+)%\]/';
+        preg_match_all($pattern, $execResult, $matches);
+        $result['mixer'] = $mixer;
+        if (is_array($matches[1])) {
+            foreach ($matches[1] as $key => $value) {
+                if (!empty($channel)) {
+                    if (in_array($value, $channel)) {
+                        $commandLine  .= $volume . '%,';
+                    }
+                    else {
+                        $commandLine .= $matches[2][$key] . '%,';
+                    }
+                }
+                else {
+                    $commandLine  .= $volume . '%,';
+                }
+            }
+            $commandLine = rtrim($commandLine, ',');
+            echo $commandLine;
+        }
+        $exec = 'sset "' . $mixer . '" ' . $commandLine;
+        $execResult = $this->amixer($exec);
+        return $execResult;
     }
 
     public function toggle($mixer, $exec = TRUE)
